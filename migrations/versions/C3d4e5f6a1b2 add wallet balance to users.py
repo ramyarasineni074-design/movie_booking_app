f4446a7082a87aa4e5d9a1469f9d0bab61ca2ebf
Ramya Rasineni <ramyarasineni@gmail.com>
2026-05-15 14:42:52 +0530

    remove duplicate payment_method migration

--- migrations/versions/C3d4e5f6a1b2 add wallet balance to users.py
+++ migrations/versions/C3d4e5f6a1b2 add wallet balance to users.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'c3d4e5f6a1b2'
-down_revision = 'b2c3d4e5f6a1'
+down_revision = 'a1b2c3d4e5f6'
 branch_labels = None
 depends_on = None
 
